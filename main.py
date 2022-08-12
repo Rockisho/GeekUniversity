d4bc04b2c09203311572da542d54db106f91e08d
Rockisho <joaoluisz@alu.ufc.br>
2022-08-12 05:04:10 -0300

    Primeiras 10 questões do curso Python Geek University

---
+++ main.py
@@ -0,0 +1,49 @@
+from math import sqrt
+
+# Questão 1
+# num = 42
+# print(num)
+
+# Questão 2
+# Num = float(1.235)
+# print(Num)
+
+# Questão 3
+# num1 = int(input("Escreva o primeiro valor"))
+# num2 = int(input("Escreva o segundo valor"))
+# num3 = int(input("Escreva o terceiro valor"))
+# soma = (num1 + num2 + num3)
+# print(soma)
+
+# Questão 4
+# raiz = float(input("Digite um número"))
+# print(raiz ** 0.5)
+
+# Questão 5
+# real = float(input("Digite um número"))
+# print(real /5)
+
+# Questão 6
+# C = float(input("Digite a temperatura em Celsius"))
+# F = float(C * (9.0) / (5.0) + 32.0)
+# print(F)
+
+# Questão 7
+# F = float(input("Digite a temperatura em Fahrenheit"))
+# C = float(5.0 *(F - 32.0) / 9.0)
+# print(C)
+
+#Questão 8
+# K = float(input("Digite a temperatura em Kelvin"))
+# C = float(K - 273.15)
+# print(C)
+
+#Questão 9
+# C = float(input("Digite a temperatura em Celsius"))
+# K = (C + 273.15)
+# print(K)
+
+#Questão 10
+# K = float(input("Digite a velocidade em KM"))
+# M = float(K / 3.6)
+# print(M)
